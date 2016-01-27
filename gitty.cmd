e51c0452d6c07414e9dd58b7ba4c8fa3f697a444
thinkingmedia <mathew@thinkingmedia.ca>
2016-01-27 11:15:20 -0500

    updated

--- gitty.cmd
+++ gitty.cmd
@@ -9,10 +9,10 @@ IF "%~1"=="" GOTO END_BATCH
 git commit -m %1
 IF %ERRORLEVEL% NEQ 0 (GOTO END_BATCH)
 
-FOR /f %%i IN ('git config --local remote.origin.url') DO SET URL=%%i
-IF x%URL:thinkingmedia=%==x%URL% GOTO PUSH_CHANGES
-SET /P AREYOUSURE=Push changes (Y/[N])?
-IF /I "%AREYOUSURE%" NEQ "Y" GOTO END_BATCH
+:: FOR /f %%i IN ('git config --local remote.origin.url') DO SET URL=%%i
+:: IF x%URL:thinkingmedia=%==x%URL% GOTO PUSH_CHANGES
+:: SET /P AREYOUSURE=Push changes (Y/[N])?
+:: IF /I "%AREYOUSURE%" NEQ "Y" GOTO END_BATCH
 :PUSH_CHANGES
 git push
 
