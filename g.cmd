42d5ad463e40319e8540b1209b07691c9d6c1dec
thinkingmedia <mathew@thinkingmedia.ca>
2016-02-03 15:46:24 -0500

    updated

--- g.cmd
+++ g.cmd
@@ -1,23 +1,26 @@
 @ECHO OFF
 SETLOCAL EnableDelayedExpansion
-SET _SHORT=0
+SET _mode=long
 IF EXIST .git (
 	FOR /F "tokens=1,2,3" %%A IN ('git remote -v') DO SET REMOTE=%%B
 	FOR %%O IN (%*) DO (
-		IF "%%O"=="-s" (
-			SET _SHORT=1
-		)
+		IF "%%O"=="-s" SET _mode=short
 	)
-	IF "!_SHORT!"=="0" (
+	
+	:: Display a single line about the status
+	IF "!_mode!"=="short" (
+		git status --short --branch
+	)
+	
+	:: Display a full report
+	IF "!_mode!"=="long" (
 		git remote -v
 		@ECHO:
 		git branch
 		@ECHO:
 		git status
-	) ELSE (
-		@ECHO [31;1m !REMOTE!
 	)
-	GOTO EXIT
+	
 ) ELSE (
 	@ECHO [31;1m Not a Git directory.
 )
