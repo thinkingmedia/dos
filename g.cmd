2f80d478299d8bb59cd666b9a2e720f30bd4991e
thinkingmedia <mathew@thinkingmedia.ca>
2016-02-03 15:34:41 -0500

    updated

--- g.cmd
+++ g.cmd
@@ -1,10 +1,24 @@
 @ECHO OFF
+SETLOCAL EnableDelayedExpansion
+SET _SHORT=0
 IF EXIST .git (
-	git remote -v
-	@ECHO:
-	git branch
-	@ECHO:
-	git status
+	FOR /F "tokens=1,2,3" %%A IN ('git remote -v') DO SET REMOTE=%%B
+	FOR %%O IN (%*) DO (
+		IF "%%O"=="-s" (
+			SET _SHORT=1
+		)
+	)
+	IF "!_SHORT!"=="0" (
+		git remote -v
+		@ECHO:
+		git branch
+		@ECHO:
+		git status
+	) ELSE (
+		@ECHO [31;1m !REMOTE!
+	)
+	GOTO EXIT
 ) ELSE (
 	@ECHO [31;1m Not a Git directory.
 )
+:EXIT
