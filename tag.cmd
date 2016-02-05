cde6fa8c22c2af69f5c8f52c7320af5a48b36a8c
thinkingmedia <mathew@thinkingmedia.ca>
2016-02-05 13:43:50 -0500

    updated

--- tag.cmd
+++ tag.cmd
@@ -1,7 +1,20 @@
+:: Handles the creating and deleting of tags.
 @ECHO OFF
+:: Show usage
 IF "%~1"=="" (
+	ECHO usage: tag [-d] ^<version^> ["message"]
+	ECHO:
+	ECHO -d    Will delete a tag on the origin
 	GOTO :EOF
 )
+:: Delete a tag
+IF "%~1"=="-d" (
+	IF "%~2"=="" GOTO :EOF
+	git tag -d %~2
+	git push origin :refs/tags/%~2
+	GOTO :EOF
+)
+:: Create a tag
 IF "%~2"=="" (
 	git tag -a %~1 -m "version %~1"
 ) ELSE (
